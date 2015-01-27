a602993f7573bcf75a3bec6f4d3c9fa8c6f07563
Thomsen <changsen4584@163.com>
2015-01-27 16:30:18 +0800

    delete opitons request, js cros angular http.post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::API
 
   def allow_cross_domain_access
     response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "GET, PUT, POST, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Methods"] = "GET, PUT, POST, DELETE"
   end
 
 end
